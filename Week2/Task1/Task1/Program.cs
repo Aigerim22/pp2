57aae8af286b094950af3d55c89bc705cf092c2d
Aigerim22 <aika0822t@gmail.com>
2019-02-06 23:44:09 +0600

    Task1

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public static string Reverse(string s)
+        {
+            char[] ch = s.ToCharArray();
+            Array.Reverse(ch);
+            return new string(ch);
+        }
+        static void Main(string[] args)
+        {
+
+            FileStream fs = new FileStream(@"C:\Users\123\Desktop\pp2\plndrm.txt", FileMode.Open,FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs);
+            string st1 = sr.ReadLine();
+            string st2=Reverse(st1);
+            
+            if (st1 == st2)
+            {
+                Console.WriteLine("Yes");
+            }
+            else Console.WriteLine("No");
+            Console.ReadKey();
+         }
+    }
+}
