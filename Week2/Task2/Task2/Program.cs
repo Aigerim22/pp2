c0f76946a31630dfe630db9168025160d2f66c8e
Aigerim22 <aika0822t@gmail.com>
2019-02-13 00:30:46 +0600

    added comments

--- Week2/Task2/Task2/Program.cs
+++ Week2/Task2/Task2/Program.cs
@@ -10,60 +10,60 @@ namespace Task2
     class Program
     {
 
-        static bool IsPrime(int x)
+        static bool IsPrime(int x) //creating bool function to check if the array of number is prime or not
         {
-            if (x == 1) return false;
-            if (x == 2) return true;
+            if (x == 1) return false; //if it is 1 and it has just one divider then it is not prime
+            if (x == 2) return true;  //if the number equals 2 so it divides to 1 and  divisible by itself then it is a prime number
 
-            bool functionResult = true;
+            bool functionResult = true; //by default it is true
 
-            for (int i = 2; i < x; ++i)
+            for (int i = 2; i < x; ++i) //running through the loop starting from 2 because every number divisible by 1
             {
-                if (x % i == 0)
+                if (x % i == 0)        //if the given number divides without reminder to i, so the given condition holds that i<x
                 {
-                    functionResult = false;
-                    break;
+                    functionResult = false; //then it is not prime
+                    break;                
                 }
             }
-            return functionResult;
+            return functionResult;    //else it is prime
         }
 
         static bool IsPrimeString(string s)
         {
-            return IsPrime(int.Parse(s));
+            return IsPrime(int.Parse(s)); //returns the string and converts to the integer
         }
 
         static void Main(string[] args)
         {
 
-            List<string> res = new List<string>();
+            List<string> res = new List<string>(); //creates list where will be saved the prime numbers
 
-            FileStream fs = new FileStream(@"C:\Users\123\Desktop\pp2\W1\input.txt", FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
+            FileStream fs = new FileStream(@"C:\Users\123\Desktop\pp2\W1\input.txt", FileMode.Open, FileAccess.Read);//reads the content from FileStream
+            StreamReader sr = new StreamReader(fs); //reads the content from byte stream
 
-            string line = sr.ReadLine();
-            string[] nums = line.Split(' ');
+            string line = sr.ReadLine();  //StreamReader reads the string line
+            string[] nums = line.Split(' '); //splited lines will be in the string array
 
-            foreach (var x in nums)
+            foreach (var x in nums)   //runs through the each number in array
             {
-                if (IsPrimeString(x))
+                if (IsPrimeString(x))  //if the called function is true
                 {
-                    res.Add(x);
+                    res.Add(x);        //then add this number to the list
                 }
             }
 
-            sr.Close();
-            fs.Close();
+            sr.Close();              //after reading closes
+            fs.Close();               
 
 
 
-            FileStream fs2 = new FileStream(@"C:\Users\123\Desktop\pp2\W1\output.txt", FileMode.Create, FileAccess.Write);
+            FileStream fs2 = new FileStream(@"C:\Users\123\Desktop\pp2\W1\output.txt", FileMode.Create, FileAccess.Write); //new FileStream in order to save the res
             StreamWriter sw = new StreamWriter(fs2);
 
-            foreach (var x in res)
+            foreach (var x in res)   //running through the each number in res
             {
-                sw.Write(x + " ");
-                Console.Write(x + " ");
+                sw.Write(x + " ");   //streamwriter adds space between each number
+                Console.Write(x + " "); 
             }
 
             sw.Close();
