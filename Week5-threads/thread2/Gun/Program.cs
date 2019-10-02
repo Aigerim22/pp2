8db42111c0800a20b149a960f8a09fc91df31079
AigerimTazhibay <47142525+Aigerim22@users.noreply.github.com>
2019-10-02 23:39:31 +0600

    Add files via upload

---
+++ Week5-threads/thread2/Gun/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                ConsoleKeyInfo cki = Console.ReadKey();
+                Console.SetCursorPosition(0, 10);
+                Console.WriteLine('O');
+
+                switch (cki.Key)
+                {
+                    case ConsoleKey.Spacebar:
+                        new Thread(DrawBullet).Start();
+                        break;
+                }
+
+                
+            }
+        }
+
+        static void DrawBullet()
+        {
+            int x = 5, y = 10;
+            while (x < 40)
+            {
+
+                Console.SetCursorPosition(x, y);
+                Console.Write('*');
+                x++;
+                Thread.Sleep(500);
+
+                Console.SetCursorPosition(x-1, y);
+                Console.Write(' ');
+            }
+        }
+    }
+}
---
+++ Week5-threads/thread2/passingdata/passingdata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+public class ThreadWithState
+{
+    private string boilerplate;
+    private int value;
+    
+    public ThreadWithState(string text, int number)
+    {
+        boilerplate = text;
+        value = number;
+    }
+    public void ThreadProc()
+    {
+        Console.WriteLine(boilerplate, value);
+    }
+}
+
+public class Example
+{
+    public static void Main()
+    {
+        ThreadWithState tws = new ThreadWithState(
+            "This report displays the number {0}.", 42);
+        Thread t = new Thread(new ThreadStart(tws.ThreadProc));
+        t.Start();
+        Console.WriteLine("Main thread does some work, then waits.");
+        t.Join();
+        Console.WriteLine(
+      "Independent task has completed; main thread ends.");
+    }
+}
