b030ec7cb9f8aee3d7ff23ca9532d45dbf00e70d
AigerimTazhibay <47142525+Aigerim22@users.noreply.github.com>
2019-10-02 23:31:38 +0600

    Add files via upload

---
+++ Week5-threads/thread2/abc/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace abc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(func);
+            Thread thread2 = new Thread(func2);
+            Thread thread3 = new Thread(func);
+            Thread thread4 = new Thread(func2);
+
+            thread.Start();
+            thread2.Start();
+            
+            thread3.Start();
+            thread4.Start();
+            
+
+        }
+        static void func()
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("abc");
+                Thread.Sleep(1000);
+            }
+        }
+        static void func2()
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine("def");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ Week5-threads/thread2/abcdeftogetheroneline/abcdeftogether.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace abcdeftogetheroneline
+{
+    class abcdeftogether
+    {
+        static void Main(string[] args)
+        { 
+            Thread thread = new Thread(F1);
+            Thread thread1 = new Thread(F2);
+            thread.Start();
+            thread1.Start();
+        }
+        public static void F1()
+        {
+            int x = 0;
+            while (true)
+            {
+                Console.SetCursorPosition(0, x++);
+                Console.Write("def");
+                Thread.Sleep(240);
+
+            }
+        }
+        public static void F2()
+        {
+            int x = 0;
+            while (true)
+            {
+                Console.SetCursorPosition(10, x++);
+                Console.Write("abc");
+                Thread.Sleep(200);
+
+            }
+        }
+    }
+}
+
