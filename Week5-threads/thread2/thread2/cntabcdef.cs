f0dea81ee0702b9105500fee5c1a9be9b9e677ad
AigerimTazhibay <47142525+Aigerim22@users.noreply.github.com>
2019-10-02 23:22:42 +0600

    Add files via upload

---
+++ Week5-threads/thread2/thread2/cntabcdef.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread2
+{
+    class cntabcdef
+    {
+        static string cnt= null;
+        static object locker = new object();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Thread thread = new Thread(F1);
+                thread.Start();
+            }
+            Console.ReadLine();
+        }
+        public static void F1()
+        {
+            lock (locker)
+            {
+                cnt = "abc";
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(cnt); 
+                    Thread.Sleep(200);
+                }
+                cnt = "def";
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine(cnt); 
+                    Thread.Sleep(500);
+                }
+            }
+        }
+    }
+}
---
+++ Week5-threads/thread2/threadabort/threadabort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+class Test
+{
+    public static void Main()
+    {
+        Thread newThread = new Thread(new ThreadStart(TestMethod));
+        newThread.Start();
+        Thread.Sleep(1000);
+
+        // Abort newThread.
+        Console.WriteLine("abc");
+        newThread.Abort("def");
+
+        // Wait for the thread to terminate.
+        newThread.Join();
+        Console.WriteLine("def");
+    }
+
+    static void TestMethod()
+    {
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine("New thread running.");
+                Thread.Sleep(1000);
+            }
+        }
+        catch (ThreadAbortException abortException)
+        {
+            Console.WriteLine((string)abortException.ExceptionState);
+        }
+    }
+}
