aa306f953fe6cd9519011583d403933c8f49ee78
AigerimTazhibay <47142525+Aigerim22@users.noreply.github.com>
2019-10-03 15:34:30 +0600

    Add files via upload

---
+++ Week5-serialization/Example1L4/studentnameid.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task1
+{
+
+   public class Program
+    {
+        public class Student
+        {
+            public string name, id;
+
+            public Student()
+            {
+            }
+
+            public Student(string name, string id)
+            {
+                this.name = name;
+                this.id = id;
+            }
+
+            public override string ToString()
+            {
+                return name + " " + id;
+            }
+
+            public static void F1()
+            {
+                string name = Console.ReadLine();
+                string id = Console.ReadLine();
+
+                Student s = new Student(name, id);
+                FileStream fs = new FileStream("task1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                XmlSerializer xs = new XmlSerializer(typeof(Student));
+                xs.Serialize(fs, s); 
+                fs.Close();
+            }
+
+            public static void F2()
+            {
+                FileStream fs = new FileStream("task1.txt", FileMode.Open, FileAccess.Read);
+                XmlSerializer xs = new XmlSerializer(typeof(Student));
+                Student s = xs.Deserialize(fs) as Student;
+                Console.WriteLine(s.ToString());
+                fs.Close();
+            }
+
+           public static void Main(string[] args)
+            {
+               F1();
+               F2();
+            }
+        }
+    }
+}
---
+++ Week5-serialization/serialization/getstudent's id/getstudentsname.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task2
+{
+    public class Program
+    {
+        public class Student
+        {
+            public string name;
+            public string id;
+
+            public Student()
+            {
+                //this.points = points;
+            }
+
+
+            public string Getname()
+            {
+                switch (id)
+                {
+                    case string n when (id == "18BD123"):
+                        name = "Aigerim";
+                        return name;
+
+                    case string n when (id == "18BD456"):
+                        name = "Alina";
+                        return name;
+                        default:
+                        return "Invalid";
+                }
+            }
+
+            public override string ToString()
+            {
+                return Getname();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+
+            F1();
+            F2();
+
+        }
+        public static void F1()
+        {
+            List<Student> objects = new List<Student>();
+            Student A = new Student();
+            A.id = Console.ReadLine();
+            objects.Add(A);
+            FileStream fs = new FileStream("marks.txt", FileMode.Create, FileAccess.Write);
+            XmlSerializer xs = new XmlSerializer(typeof(List<Student>));
+            xs.Serialize(fs, objects);
+            fs.Close();
+        }
+
+        public static void F2()
+        {
+            FileStream fs = new FileStream("marks.txt", FileMode.Open, FileAccess.Read);
+            XmlSerializer xs = new XmlSerializer(typeof(List<Student>));
+            List<Student> objects = xs.Deserialize(fs) as List<Student>;
+            for (int i = 0; i < objects.Count; i++)
+            {
+                Console.WriteLine(objects[i]);
+            }
+            Console.WriteLine(objects.ToString());
+            fs.Close();
+        }
+    }
+}
